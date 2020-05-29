8c3f5cf74512f436fa615aefa7b042a5c9d84a8a
moonbeam87 <2021dpatel@gmail.com>
2020-05-29 14:51:11 -0400

    minor updates

--- snakewm/apps/tools/stocks/term.py
+++ snakewm/apps/tools/stocks/term.py
@@ -30,7 +30,18 @@ class stocks(pygame_gui.elements.UIWindow):
                 'bottom': 'bottom'
             }
         )
-        print("Enter Name of Stock to Continue!")
+        self.textbox = pygame_gui.elements.UITextBox(
+            "",
+            relative_rect=pygame.Rect(0, 60, 368, 200),
+            manager=manager,
+            container=self,
+            anchors={
+                'left': 'left',
+                'right': 'right',
+                'top': 'top',
+                'bottom': 'bottom'
+            }
+        )
         self.input = pygame_gui.elements.UITextEntryLine(
             relative_rect=pygame.Rect(0, -35, 368, 30),
             manager=manager,
@@ -42,5 +53,34 @@ class stocks(pygame_gui.elements.UIWindow):
                 'bottom': 'bottom'
             }
         )
+        
+    def set_text(self, text):
+        self.textbox.html_text = text.replace('\n', '<br>')
+        self.textbox.rebuild()
 
+    def append_text(self, text):
+        self.textbox.html_text = self.textbox.html_text + text.replace('\n', '<br>')
+        self.textbox.rebuild()
 
+    def process_event(self, event):
+        super().process_event(event)
+
+        if event.type == pygame.KEYUP and event.key == pygame.K_RETURN:
+            _stdout = sys.stdout
+            sys.stdout = tout = StringIO()
+
+            try:
+                code = compile(self.input.get_text(), 'snaketerm_code', 'exec')
+                exec(code, globals())
+            except Exception:
+                e_type,e_val,e_traceback = sys.exc_info()
+                print('Traceback (most recent call last):')
+                traceback.print_tb(e_traceback, None, tout)
+                print(e_type, e_val)
+
+            sys.stdout = _stdout
+            result = tout.getvalue()
+            self.append_text(result)
+            self.input.set_text('')
+
+   
